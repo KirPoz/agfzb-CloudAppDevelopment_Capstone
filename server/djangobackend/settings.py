e4e4135e6ac62801218814025d5b10f7022d681b
Kirill Poznyak <kirill.poznyak@gmail.com>
2021-12-30 19:07:55 +0000

    changes made from theia lab environment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["dealershipkp.eu-gb.mybluemix.net"]
+ALLOWED_HOSTS = ["dealershipkp.eu-gb.mybluemix.net", "localhost"]
 
 
 # Application definition
