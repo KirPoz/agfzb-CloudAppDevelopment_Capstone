d53601906de09f19be628c44bd4fcd1fbe5d385c
K P <kirill.poznyak@gmail.com>
2022-01-18 18:31:35 +0000

    changes made from theia lab environment

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,12 @@
-from django.db import models
+
+import sys
 from django.utils.timezone import now
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
+from django.conf import settings
 
 
 # Create your models here.
@@ -10,6 +17,14 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    care_name = models.CharField(max_length=1000)
+    car_description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "Name: " + self.car_name + "," + \
+               "Description: " + self.car_description
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +34,31 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    care_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    care_name = models.CharField(max_length=1000)
+    dealer_id = models.IntegerField(null=False)
+    SEDAN = 'sedan'
+    SUS = 'sus'
+    WAGON = 'wagon'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUS, 'SUS'),
+        (WAGON, 'WAGON')
+    ]
+    care_type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+    car_year = models.DateField(null=False)
+
+    def __str__(self):
+        return "Name: " + self.car_name
+
+
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
